56ab7e5b5dfc4e50550942013bbe3748a6052f12
djpdim <dimospagkas@yahoo.gr>
2022-05-08 21:14:14 -0400

    update code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,40 @@
-$(document).ready(function() {
-    $('.saveBtn').on("click", function() {
-        var value = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time, value);
-    });
+$(document).ready(function () {
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, value)
+    })
 
     function hourUpdate() {
-        var currentHour = moment().hours();
-        $(".time-block").each(function() {
-            var blockHour = parseInt($(this).attr("id").split("-")[1]);
+        var currentHour = moment().hours()
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("-")[1])
 
             if (blockHour < currentHour) {
-                $(this).addClass("past");
+                $(this).addClass("past")
             } else if (blockHour === currentHour) {
-                $(this).removeClass("past");
-                $(this).addClass("present");
+                $(this).removeClass("past")
+                $(this).addClass("present")
             } else {
-                $(this).removeClass("past");
-                $(this).removeClass("present");
-                $(this).addClass("future");
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
             }
-
-        });
-    };
+        })
+    }
     hourUpdate()
 
-    var interval = setInterval(hourUpdate, 15000);
-    $("#hour-9 .description").val(localStorage.getItem("hour-9"));
-    $("#hour-10 .description").val(localStorage.getItem("hour-10"));
-    $("#hour-11 .description").val(localStorage.getItem("hour-11"));
-    $("#hour-12 .description").val(localStorage.getItem("hour-12"));
-    $("#hour-13 .description").val(localStorage.getItem("hour-13"));
-    $("#hour-14 .description").val(localStorage.getItem("hour-14"));
-    $("#hour-15 .description").val(localStorage.getItem("hour-15"));
-    $("#hour-16 .description").val(localStorage.getItem("hour-16"));
-    $("#hour-17 .description").val(localStorage.getItem("hour-17"));
-    $("#hour-18 .description").val(localStorage.getItem("hour-18"));
+    var interval = setInterval(hourUpdate, 15000)
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+    $("#hour-18 .description").val(localStorage.getItem("hour-18"))
 
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
-
-});
+    $("#currentDay").text(moment().format("dddd, MMMM Do"))
+})
